398ded007679d2b10d3f62b5669405e4a6dd097f
musaomark01 <musaomark01@gmail.com>
2024-01-04 18:16:37 -0500

    Added  a resume page

---
+++ src/components/pages/resume.jsx
@@ -0,0 +1,10 @@
+export default function Resume() {
+    return (
+        <section>
+            <h2> Resume</h2>
+            <p>
+                Coming soon!
+            </p>
+        </section>
+    );
+}
